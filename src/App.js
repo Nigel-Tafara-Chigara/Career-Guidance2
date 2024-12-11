a8f37a0ba4ecad027800ba55872cfec312924315
nkosilathi <nkosilatimosey@gmail.com>
2024-12-11 15:17:01 +0200

    finished the frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Hello from "./components/register";
+import Login from "./components/log";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Mainpage from "./components/mainpg";
+import LandingPage from "./components/LandingPage";
+import ProgramDisplay from "./components/ProgramDisplay";
+import Login1 from "./components/logi";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />}></Route>
+
+        <Route path="/logi" element={<Login1 />}></Route>
+        <Route path="/log" element={<Login />}></Route>
+        <Route path="/register" element={<Hello />}></Route>
+        <Route path="/ProgramDisplay" element={<ProgramDisplay />}></Route>
+        <Route path="/mainpg" element={<Mainpage />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
